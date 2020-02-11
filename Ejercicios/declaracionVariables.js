2e6be008eb17948edfd5ab55538c513101bc174d
505 <505@academicos.cesde.edu.co>
2020-02-10 19:45:49 -0500

    Declaración de variables explicación

---
+++ Ejercicios/declaracionVariables.js
@@ -0,0 +1,16 @@
+//antigua forma de declarar variables
+//var nombre = "andrea";
+//var nombre = "camilo";
+
+let nombre = "andrea2";
+console.log(nombre);
+
+let costo = 5;
+if (costo > 3) {
+    let comida = "galleta";
+}
+console.log("la comida " + comida);
+
+function comer(params) {
+    var cosa = "cosas";
+}
